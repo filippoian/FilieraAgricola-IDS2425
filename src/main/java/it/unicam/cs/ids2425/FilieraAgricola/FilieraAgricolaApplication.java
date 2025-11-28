6547b1d02b28b9ffc31e24d561dd119402dcc236
Filippo <155171675+filippoian@users.noreply.github.com>
2025-11-28 11:29:20 +0100

    Setup progetto

--- src/main/java/it/unicam/cs/ids2425/FilieraAgricola/FilieraAgricolaApplication.java
+++ src/main/java/it/unicam/cs/ids2425/FilieraAgricola/FilieraAgricolaApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FilieraAgricolaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FilieraAgricolaApplication.class, args);
-	}
 
+		SpringApplication.run(FilieraAgricolaApplication.class, args);
+
+		System.out.println("FilieraAgricolaApplication started");
+	}
 }
