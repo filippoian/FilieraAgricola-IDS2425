968901bee1cff3bf85480bcaaa8254f04ef72feb
Filippo <155171675+filippoian@users.noreply.github.com>
2025-11-28 10:52:15 +0100

    First commit: inizializzazione progetto FilieraAgricola

---
+++ src/main/java/it/unicam/cs/ids2425/FilieraAgricola/FilieraAgricolaApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids2425.FilieraAgricola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilieraAgricolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilieraAgricolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unicam/cs/ids2425/FilieraAgricola/FilieraAgricolaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids2425.FilieraAgricola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilieraAgricolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
