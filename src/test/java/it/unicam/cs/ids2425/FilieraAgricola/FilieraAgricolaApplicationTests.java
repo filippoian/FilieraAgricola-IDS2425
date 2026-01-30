0310774b20e057c14a4c282f9b9ef36baabc017b
vincenzo mattioli <m3tthius@gmail.com>
2026-01-30 17:55:07 +0100

    Test e Build Tools

--- src/test/java/it/unicam/cs/ids2425/FilieraAgricola/FilieraAgricolaApplicationTests.java
+++ src/test/java/it/unicam/cs/ids2425/FilieraAgricola/FilieraAgricolaApplicationTests.java
@@ -1,13 +1,24 @@
+
 package it.unicam.cs.ids2425.FilieraAgricola;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+// Verifica che l'intero contesto dell'applicazione Spring Boot si carichi correttamente.
 @SpringBootTest
+// Utilizziamo il profilo di test per caricare le configurazioni specifiche (es.
+// database in memoria)
+// definite in application-test.properties.
+@ActiveProfiles("test")
+
 class FilieraAgricolaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Se il metodo non lancia eccezioni, significa che i bean principali e la
+		// configurazione
+		// del database sono stati inizializzati correttamente.
 	}
 
 }
